c16cf41a804a073e8f4176a591c98a84edd03039
MarMinsk <mwheatley@hotmail.com>
2017-06-05 10:53:48 +0100

    working through f-e authentication

---
+++ src/js/services/token.service.js
@@ -0,0 +1,12 @@
+angular
+.module('MarketApp')
+.service('TokenService', TokenService);
+
+TokenService.$inject = ['$window'];
+function TokenService($window) {
+  const self = this;
+
+  self.setToken = (token) => {
+    return $window.localStorage.setItem('auth-token', token);
+  };
+}
