2c6685bb37c794f852493a836152dda37975aaf3
MarMinsk <mwheatley@hotmail.com>
2017-06-05 17:01:44 +0100

    authentication problem fixed: git add -A

--- src/js/controllers/login.controller.js
+++ src/js/controllers/login.controller.js
@@ -2,8 +2,8 @@ angular
 .module('MarketApp')
 .controller('LoginCtrl', LoginCtrl);
 
-LoginCtrl.$inject = ['User', 'CurrentUserService'];
-function LoginCtrl(User, CurrentUserService) {
+LoginCtrl.$inject = ['User', 'CurrentUserService', '$state'];
+function LoginCtrl(User, CurrentUserService, $state) {
   const vm = this;
 
   vm.login = () => {
@@ -11,6 +11,7 @@ function LoginCtrl(User, CurrentUserService) {
     .login(vm.user).$promise
     .then(() => {
       CurrentUserService.getUser();
+      $state.go('usersIndex');
     }, err => {
       console.log(err);
     });
