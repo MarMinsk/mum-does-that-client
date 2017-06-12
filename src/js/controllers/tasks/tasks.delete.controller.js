3c2427a71854f6351f8346fd09a97cc3ec67c971
MarMinsk <mwheatley@hotmail.com>
2017-06-12 11:24:12 +0100

    matteo fixed home page format problem

--- src/js/controllers/tasks/tasks.delete.controller.js
+++ src/js/controllers/tasks/tasks.delete.controller.js
@@ -8,14 +8,5 @@ function TasksDeleteCtrl(Task, $state) {
   var vm = this;
   vm.task = Task;
 
-  function tasksDelete(task) {
-    console.log('task delete clicked');
-    vm.task
-      .$remove({id: task.id})
-      .$promise
-      .then(() => {
-        $state.go('tasksIndex');
-      });
-  }
-  vm.delete = tasksDelete;
+
 }
