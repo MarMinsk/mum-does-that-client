f0fa1291a12aae2002bd9244c2c7fc1c0f0c1791
MarMinsk <mwheatley@hotmail.com>
2017-06-12 14:03:47 +0100

    updating user skills edit form

--- src/js/controllers/users/users.edit.controller.js
+++ src/js/controllers/users/users.edit.controller.js
@@ -2,21 +2,39 @@ angular
 .module('MarketApp')
 .controller('UsersEditCtrl', UsersEditCtrl);
 
-UsersEditCtrl.$inject = ['User', '$stateParams', '$state' ];
-function UsersEditCtrl(User, $stateParams, $state) {
+UsersEditCtrl.$inject = ['User', 'Skill', '$stateParams', '$state' ];
+function UsersEditCtrl(User, Skill, $stateParams, $state) {
   const vm    = this;
 
-  vm.user     = User.get($stateParams);
-  console.log('USERS EDIT PAGE', vm.user);
+  User
+  .get($stateParams)
+  .$promise
+  .then(user => {
+    vm.user = user;
+    vm.user.skill_ids = vm.user.skills.map(skill => skill.id);
+  }, err => {
+    console.error(err);
+  });
+
+  vm.skills   = Skill.query();
+
   vm.update   = usersUpdate;
 
   function usersUpdate() {
     User
-      .update({ id: $stateParams.id }, vm.user)
+      .update({ id: $stateParams.id }, { user: vm.user })
       .$promise
       .then(user => {
         $state.go('usersShow', { id: user.id });
       });
 
   }
+
+  vm.increaseSkillCount = () => {
+    vm.user.skill_ids.push(undefined);
+  };
+
+  vm.decreaseSkillCount = () => {
+    vm.user.skill_ids.pop();
+  };
 }
